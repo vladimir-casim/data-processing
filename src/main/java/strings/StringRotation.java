c40f5381adc82357a07f3bdbc2c4bbec8cdcdf30
Vladimir <casim.vladimir@gmail.com>
2017-07-19 11:36:04 +0300

    Added examples

---
+++ src/main/java/strings/StringRotation.java
@@ -0,0 +1,24 @@
+package strings;
+
+/**
+ * Check Whether One String Is Rotation Of Another?
+ */
+public class StringRotation {
+
+    public static void main(String[] args) {
+        String s1 = "JavaJ2eeStrutsHibernate";
+        String s2 = "StrutsHibernateJavaJ2ee";
+
+        if (s1.length() != s2.length()) {
+            System.out.println("s2 is not rotated version of s1");
+        } else {
+            String s3 = s1 + s1;
+            if (s3.contains(s2)) {
+                System.out.println("s2 is a rotated version of s1");
+            } else {
+                System.out.println("s2 is not rotated version of s1");
+            }
+        }
+    }
+
+}
