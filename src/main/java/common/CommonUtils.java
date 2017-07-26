b90698a5dd0cc17f3927e8d257a36b6a10cd8bf8
Vladimir <casim.vladimir@gmail.com>
2017-07-26 14:44:37 +0300

    Added examples

---
+++ src/main/java/common/CommonUtils.java
@@ -0,0 +1,24 @@
+package common;
+
+import java.util.*;
+
+public class CommonUtils {
+
+    public static void main(String[] args) {
+        Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+
+        Integer[] moreInts = { 6, 7, 8, 9, 10 };
+        collection.addAll(Arrays.asList(moreInts));
+
+        Collections.addAll(collection, 11, 12, 13, 14, 15);
+        Collections.addAll(collection, moreInts);
+
+        List<Integer> list = Arrays.asList(16, 17, 18, 19, 20);
+        list.set(1, 99);
+        // arraylist.add(21);
+    }
+
+    public static void  sampleMethod(String str) {
+        Objects.requireNonNull(str, "str must not be null");
+    }
+}
---
+++ src/main/java/common/RandomNumbers.java
@@ -0,0 +1,35 @@
+package common;
+
+import java.util.Random;
+
+/**
+ * Example of random generated numbers
+ *
+ * Created by Vladimir
+ */
+
+public class RandomNumbers {
+
+    public static void main(String[] args) {
+        //range from min till max
+        int min = 5;
+        int max = 10;
+        int randomNumber;
+
+        // Math.random() generates a double value in the range [0,1)
+        // (int) truncate the decimal part
+        randomNumber = min + (int)(Math.random() * ((max - min) + 1));
+        System.out.println(randomNumber);
+
+        Random rand = new Random(100);
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        randomNumber = rand.nextInt((max - min) + 1) + min;
+        System.out.println(randomNumber);
+
+        randomNumber = rand.nextInt(max);
+        System.out.println(randomNumber);
+    }
+
+}
