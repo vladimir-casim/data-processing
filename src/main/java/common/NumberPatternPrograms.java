ebcf10bfd314ddbbfccb783352e97ea64067a4a9
Vladimir <casim.vladimir@gmail.com>
2017-08-22 17:00:59 +0300

    Added examples

--- src/main/java/common/NumberPatternPrograms.java
+++ src/main/java/common/NumberPatternPrograms.java
@@ -56,13 +56,82 @@ public class NumberPatternPrograms {
         System.out.println("How many rows you want in this pattern?");
         int rows = scanner.nextInt();
 
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+
+    /*
+    1 2 3 4
+    1 2 3
+    1 2
+    1
+     */
+    static void pattern4() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows you want in this pattern?");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    4 3 2 1
+    4 3 2
+    4 3
+    4
+     */
+    static void pattern5() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows you want in this pattern?");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    4
+    4 3
+    4 3 2
+    4 3 2 1
+     */
+    static void pattern6() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows you want in this pattern?");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = rows; j >= i; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
-        //pattern1();
-        //pattern2();
-        pattern3();
+        pattern6();
     }
 
 }
