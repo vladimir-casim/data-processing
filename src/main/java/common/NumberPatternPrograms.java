0d2631e63f376c98aa02fa606de2f5d85e6ad25c
Vladimir <casim.vladimir@gmail.com>
2017-09-18 10:58:20 +0300

    Added examples

--- src/main/java/common/NumberPatternPrograms.java
+++ src/main/java/common/NumberPatternPrograms.java
@@ -130,8 +130,56 @@ public class NumberPatternPrograms {
         }
     }
 
+    /*
+    4 3 2 1
+    3 2 1
+    2 1
+    1
+     */
+    static void pattern7() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows you want in this pattern?");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*
+    1 2 3 4
+    1 2 3
+    1 2
+    1
+    1 2
+    1 2 3
+    1 2 3 4
+     */
+    static void pattern8() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many rows you want in this pattern?");
+        int rows = scanner.nextInt();
+
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 2; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        pattern6();
+        pattern8();
     }
 
 }
